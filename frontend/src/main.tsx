324daeea001de69a033caf57fca04be8ae7a449e
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-07 13:06:51 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./i18n";
+import { useThemeStore } from "./store/themeStore";
 
 // Initialize theme on app load
 const initializeTheme = () => {

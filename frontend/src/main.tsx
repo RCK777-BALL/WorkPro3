077abba1a156fa64b0ffefa2660d2f3f5503cd4e
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-07 09:31:51 -0400

    refactor: use React default import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 // frontend/src/main.tsx
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -11,7 +11,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );

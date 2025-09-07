210753077d3aa5fff5b3b216e085f5a0a97f2a7e
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-07 12:46:46 -0400

    refactor: add single router and i18n init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
-// src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./i18n";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element with id 'root' not found");
 
 createRoot(rootEl).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
+

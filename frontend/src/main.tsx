13502502595af1053bc42cabaadd438fbcb3196b
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-07 03:58:06 -0400

    3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-function App() {
-  return <h1>Hello WorkPro</h1>;
-}
-
-const rootElement = document.getElementById('root')!;
-createRoot(rootElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );

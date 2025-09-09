2d8af41429227de013747c98b9ec220acdb19b14
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-09 03:26:54 -0400

    chore: enable strict types and add linting

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist/', 'node_modules/'],
+};

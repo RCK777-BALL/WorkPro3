28e1eb9b75c7406407a0ab9d1d252f59b2d591bd
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-11 18:17:19 -0400

    feat: add modals alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@/common': fileURLToPath(new URL('./src/components/common', import.meta.url)),
+      '@/modals': fileURLToPath(new URL('./src/components/modals', import.meta.url)),
     },
   },
 })

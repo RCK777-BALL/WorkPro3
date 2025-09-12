3c73e6ebe288839d77f3a07f083f674593584ab9
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-12 09:38:04 -0400

    Add AuthProvider to main entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,15 +8,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import App from './App';
 import AppErrorBoundary from './components/common/AppErrorBoundary';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MantineProvider>
-        <AppErrorBoundary>
-          <App />
-        </AppErrorBoundary>
-      </MantineProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <MantineProvider>
+          <AppErrorBoundary>
+            <App />
+          </AppErrorBoundary>
+        </MantineProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );

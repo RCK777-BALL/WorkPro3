c3d08d67911e1e2fe475ca939c0cc32914308d5a
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-12 09:39:47 -0400

    feat: add global types for signature canvas

---
+++ frontend/src/types/globals.d.ts
@@ -0,0 +1,25 @@
+declare module "react-signature-canvas" {
+  import * as React from "react";
+  export interface SignatureCanvasProps extends React.CanvasHTMLAttributes<HTMLCanvasElement> {
+    penColor?: string;
+    backgroundColor?: string;
+    throttle?: number;
+    velocityFilterWeight?: number;
+    canvasProps?: React.CanvasHTMLAttributes<HTMLCanvasElement>;
+    clearOnResize?: boolean;
+  }
+  export default class SignatureCanvas extends React.Component<SignatureCanvasProps> {
+    clear(): void;
+    isEmpty(): boolean;
+    fromDataURL(dataURL: string, options?: { ratio?: number; width?: number; height?: number }): void;
+    toDataURL(type?: string, encoderOptions?: number): string;
+  }
+}
+
+interface SyncManager {
+  register(tag: string): Promise<void>;
+  getTags?(): Promise<string[]>;
+}
+interface ServiceWorkerRegistration {
+  sync?: SyncManager;
+}

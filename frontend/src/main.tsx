21b6341715dcb600eb77986fa765e41a8a013fda
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-09-13 14:45:30 -0400

    Simplify main bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,21 +5,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
-import AppErrorBoundary from './components/common/AppErrorBoundary';
-import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <AppErrorBoundary>
-            <App />
-          </AppErrorBoundary>
-        </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );

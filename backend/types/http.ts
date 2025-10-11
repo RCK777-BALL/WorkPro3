8cb5ec9e181e5b842b2e6ba28749f8b4dc98d6e0
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-10-11 09:10:49 -0400

    Refine backend HTTP typings

--- backend/types/http.ts
+++ backend/types/http.ts
@@ -2,4 +2,28 @@
  * SPDX-License-Identifier: MIT
  */
 
-export { AuthedRequestHandler, AuthedRequest, ApiResult } from '@shared/http';
+import type { Request, Response, NextFunction } from 'express';
+import type { ParamsDictionary } from 'express-serve-static-core';
+import type { ParsedQs } from 'qs';
+
+export type { ApiResult } from '@shared/http';
+
+export type AuthedRequest<
+  P extends ParamsDictionary = ParamsDictionary,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery extends ParsedQs = ParsedQs,
+> = Request<P, ResBody, ReqBody, ReqQuery> & {
+  user: Express.User;
+};
+
+export type AuthedRequestHandler<
+  P extends ParamsDictionary = ParamsDictionary,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery extends ParsedQs = ParsedQs,
+> = (
+  req: AuthedRequest<P, ResBody, ReqBody, ReqQuery>,
+  res: Response<ResBody>,
+  next: NextFunction,
+) => unknown | Promise<unknown>;

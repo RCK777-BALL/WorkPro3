609a492b491ac8ea76641beb06a51ede9cdc5541
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-10-14 09:51:36 -0400

    fix: repair tailwind and toast setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -22,12 +22,12 @@ if (typeof window !== 'undefined' && typeof window.initNavBar === 'function') {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
         <Toaster position="top-right" />
-      </>
-    </AuthProvider>
-  </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );

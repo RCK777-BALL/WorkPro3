4f8a77e853e82e857d2095827f756dac9565bd62
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-10-22 04:05:54 -0400

    ][

--- Frontend/src/lib/api.test.ts
+++
@@ -1,9 +0,0 @@
-import { expectTypeOf, test } from 'vitest';
-import { fetchJson } from './api';
-import type { ApiResult } from '@shared/http';
-
-test('fetchJson returns ApiResult', () => {
-  expectTypeOf(fetchJson<{ ok: boolean }>('/test')).toEqualTypeOf<Promise<ApiResult<{ ok: boolean }>>>();
-  // @ts-expect-error fetchJson should not return raw data
-  expectTypeOf(fetchJson<{ ok: boolean }>('/test')).toEqualTypeOf<Promise<{ ok: boolean }>>();
-});

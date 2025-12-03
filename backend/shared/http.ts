2ba587e6b23ce436c94c87c9657335023dbb0f06
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2025-12-02 20:40:50 -0500

    4

--- backend/shared/http.ts
+++
@@ -1,8 +0,0 @@
-// shared/http.ts
-
-export type ApiResult<T = unknown> = {
-  ok: boolean;
-  data?: T | undefined;
-  error?: string | string[] | null | undefined;
-  status?: number | undefined;
-};

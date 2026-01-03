dbc9c319331e56aaa4f4f6bedcbc0212aa8d7ee2
RCK777-BALL <66157079+RCK777-BALL@users.noreply.github.com>
2026-01-03 05:39:48 -0500

    Re-export notification helpers

--- backend/src/modules/notifications/index.ts
+++ backend/src/modules/notifications/index.ts
@@ -3,3 +3,10 @@
  */
 
 export { default as notificationsModuleRouter } from './router';
+export {
+  deleteUserSubscription,
+  listUserSubscriptions,
+  notifyWorkOrderSlaBreach,
+  notifyWorkOrderSlaEscalation,
+  upsertUserSubscription,
+} from './service';
